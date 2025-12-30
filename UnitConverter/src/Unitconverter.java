b90a2191e61333dd9e14262fb71e96ee26b94a2b
Azarudeen <azarabdullah05@gmail.com>
2025-12-31 01:06:31 +0530

    Add files via upload
    Initial commit: Added Unitconverter project (java)

---
+++ UnitConverter/src/Unitconverter.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Unitconverter {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("===== Simple Unit Converter =====");
+        System.out.println("1. Kilometers to Miles");
+        System.out.println("2. Miles to Kilometers");
+        System.out.println("3. Celsius to Fahrenheit");
+        System.out.println("4. Fahrenheit to Celsius");
+
+        System.out.print("Choose option (1-4): ");
+        int option = sc.nextInt();
+
+        System.out.print("Enter value to convert: ");
+        double value = sc.nextDouble();
+        double result = 0;
+
+        switch(option) {
+            case 1: // Km to Miles
+                result = value * 0.621371;
+                System.out.println(value + " km = " + result + " miles");
+                break;
+            case 2: // Miles to Km
+                result = value / 0.621371;
+                System.out.println(value + " miles = " + result + " km");
+                break;
+            case 3: // Celsius to Fahrenheit
+                result = (value * 9/5) + 32;
+                System.out.println(value + "°C = " + result + "°F");
+                break;
+            case 4: // Fahrenheit to Celsius
+                result = (value - 32) * 5/9;
+                System.out.println(value + "°F = " + result + "°C");
+                break;
+            default:
+                System.out.println("Invalid option. Please choose 1-4.");
+        }
+    }
+}
